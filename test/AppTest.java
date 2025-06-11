15c249992a07e175ff4d3fef44f0ebb9b7943e10
Dragos Mihalcea <dragos.mihalcea@luxon-tech.com>
2025-06-11 15:58:23 +0300

    Added tests

--- test/AppTest.java
+++ test/AppTest.java
@@ -1,8 +1,18 @@
-public class LogMonitoringAppTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.io.IOException;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
 
   @Test
-  void testLogParser_parsesCorrectly() throws IOException {
-    // Arrange
+  void testLogParserParsesCorrectly() throws IOException {
+
     String content = """
             10:00:00, Job A, START, 101
             10:10:00, Job A, END, 101
@@ -14,20 +24,17 @@ public class LogMonitoringAppTest {
     Files.writeString(tempFile, content);
 
     LogParser parser = new LogParser();
-
-    // Act
     List<LogEvent> events = parser.parse(tempFile.toString());
 
-    // Assert
     assertEquals(4, events.size());
     assertEquals("Job A", events.get(0).description);
-    assertEquals("START", events.get(0).action);
+    assertEquals("START", events.get(2).type);
     assertEquals(101, events.get(0).pid);
   }
 
   @Test
-  void testJobMonitor_matchesStartAndEnd() {
-    // Arrange
+  void testJobMonitorMatchesStartAndEnd() {
+
     List<LogEvent> events = List.of(
         new LogEvent("10:00:00", "Job A", "START", 1),
         new LogEvent("10:05:00", "Job A", "END", 1),
@@ -36,39 +43,30 @@ public class LogMonitoringAppTest {
     );
 
     JobMonitor monitor = new JobMonitor();
-
-    // Act
     List<JobInfo> jobs = monitor.analyze(events);
 
-    // Assert
     assertEquals(2, jobs.size());
     assertEquals(Duration.ofMinutes(5), jobs.get(0).getDuration());
     assertEquals(Duration.ofMinutes(20), jobs.get(1).getDuration());
   }
 
   @Test
-  void testJobMonitor_ignoresUnpairedEnds() {
-    // Arrange
+  void testJobMonitorIgnoresUnpairedEnds() {
+
     List<LogEvent> events = List.of(
         new LogEvent("10:00:00", "Job A", "END", 1) // no START
     );
 
     JobMonitor monitor = new JobMonitor();
-
-    // Act
     List<JobInfo> jobs = monitor.analyze(events);
 
-    // Assert
     assertEquals(0, jobs.size());
   }
 
   @Test
-  void testJobInfo_durationIsCorrect() {
-    JobInfo job = new JobInfo(
-        42,
-        "Sample",
-        LocalTime.parse("10:00:00"),
-        LocalTime.parse("10:15:00")
+  void testJobInfoDurationIsCorrect() {
+    JobInfo job = new JobInfo(42, "scheduled task test",
+        LocalTime.parse("10:00:00"), LocalTime.parse("10:15:00")
     );
 
     assertEquals(Duration.ofMinutes(15), job.getDuration());
